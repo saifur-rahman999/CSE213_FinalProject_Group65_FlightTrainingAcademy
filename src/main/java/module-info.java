eb4ede745527db242b5eb2c9b395f4c27dd0fcef
saifur-rahman999 <saifanik1139@gmail.com>
2025-08-14 23:36:33 +0600

    Initial project setup

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,17 @@
 module com.example.cse213_finalproject_group65_flighttrainingacademy {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens com.example.cse213_finalproject_group65_flighttrainingacademy to javafx.fxml;
+    opens com.example.cse213_finalproject_group65_flighttrainingacademy.AircraftMaintenanceEngineer to javafx.fxml, javafx.base;
+    opens com.example.cse213_finalproject_group65_flighttrainingacademy.AtcCoordinator to javafx.fxml, javafx.base;
+    opens com.example.cse213_finalproject_group65_flighttrainingacademy.CertifiedExaminer to javafx.fxml, javafx.base;
+    opens com.example.cse213_finalproject_group65_flighttrainingacademy.FinanceAndEnrollmentOfficer to javafx.fxml, javafx.base;
+    opens com.example.cse213_finalproject_group65_flighttrainingacademy.FlightInstructor to javafx.fxml, javafx.base;
+    opens com.example.cse213_finalproject_group65_flighttrainingacademy.GroundInstructor to javafx.fxml, javafx.base;
+    opens com.example.cse213_finalproject_group65_flighttrainingacademy.OperationManager to javafx.fxml, javafx.base;
+    opens com.example.cse213_finalproject_group65_flighttrainingacademy.TraineePilot to javafx.fxml, javafx.base;
     exports com.example.cse213_finalproject_group65_flighttrainingacademy;
 }
