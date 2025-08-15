3f2511ec7827f2286a2ab8d4be7eb51ab42e6413
saifur-rahman999 <saifanik1139@gmail.com>
2025-08-16 02:10:19 +0600

    hh

--- src/main/java/com/example/cse213_finalproject_group65_flighttrainingacademy/GroundInstructor/GroundInstructor.java
+++ src/main/java/com/example/cse213_finalproject_group65_flighttrainingacademy/GroundInstructor/GroundInstructor.java
@@ -5,8 +5,32 @@ import com.example.cse213_finalproject_group65_flighttrainingacademy.User;
 import java.time.LocalDate;
 
 public class GroundInstructor extends User {
+    private String specialization;
 
-    public GroundInstructor(String id, String name, String phoneNo, String email, String address, String gender, String password, LocalDate dob) {
+    public GroundInstructor(String id, String name, String phoneNo, String email, String address, String gender, String password, LocalDate dob, String specialization) {
+
         super(id, name, phoneNo, email, address, gender, password, dob);
+        this.specialization = specialization;
     }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+
+    @Override
+    public String toString() {
+        return "GroundInstructor{" +
+                "specialization='" + specialization + '\'' +
+                '}';
+    }
+
+    GroundInstructor gi = new GroundInstructor(
+            "0005", "Vladimir Putin", "01711540789", "nazmul@gmail.com",
+            "Dhaka", "Male", "ginstructor", LocalDate.of(2001, 5, 14),
+            "Air Law");
+
 }
