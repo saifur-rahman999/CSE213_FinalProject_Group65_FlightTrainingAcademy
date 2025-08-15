ff22b66a7718c6dabe7d22e95cd83222ec24a05f
anudu <2321051@iub.edu.bd>
2025-08-15 16:37:31 +0300

    a

--- src/main/java/com/example/cse213_finalproject_group65_flighttrainingacademy/TraineePilot/ViewSchedule.java
+++ src/main/java/com/example/cse213_finalproject_group65_flighttrainingacademy/TraineePilot/ViewSchedule.java
@@ -1,4 +1,41 @@
 package com.example.cse213_finalproject_group65_flighttrainingacademy.TraineePilot;
 
+import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
+
 public class ViewSchedule {
+    @javafx.fxml.FXML
+    private HBox FilterHBox;
+    @javafx.fxml.FXML
+    private TableColumn flightTimeCo;
+    @javafx.fxml.FXML
+    private TableColumn flightDateCo;
+    @javafx.fxml.FXML
+    private TableColumn trauneeIDco;
+    @javafx.fxml.FXML
+    private TableView tableView;
+    @javafx.fxml.FXML
+    private TableColumn aircraftTypeCo;
+    @javafx.fxml.FXML
+    private TableColumn trainingLocCo;
+    @javafx.fxml.FXML
+    private Label dateRangeValidationLabel;
+    @javafx.fxml.FXML
+    private TableColumn sessionidco;
+    @javafx.fxml.FXML
+    private ComboBox sessionTypeComboBox;
+    @javafx.fxml.FXML
+    private DatePicker StartDatePicker;
+    @javafx.fxml.FXML
+    private DatePicker endDatePicker;
+    @javafx.fxml.FXML
+    private TableColumn sessionTypeCo;
+
+    @javafx.fxml.FXML
+    public void exportScheduleOnClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void ApplyFilterOnClick(ActionEvent actionEvent) {
+    }
 }
